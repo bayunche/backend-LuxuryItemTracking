057628a6da8bada3f7976066554240589bad47e1
bayunche <1035010167@qq.com>
2023-11-21 10:15:01 +0800

    feat login complete        DBconnection complete

--- client/router/login/login.js
+++ client/router/login/login.js
@@ -1,29 +1,55 @@
-const login = require("../../tools/data/models/login")
-const {genrateToken} = require("../../utils/jwtCheck")
-const {argon,verify}=require("../../utils/argon")
+const { now } = require("moment/moment");
+const { connectDatabase, getAllUsers } = require("../../data/UserDatabase");
+const User = require("../../data/entity/user");
+const { ulid } = require("ulid");
 
-export function login (req, res) {
-    let { userName, password } = req.body
-    let passwordF = argon(password)
-    let condition = 1
-    // user.find({ userName, passWordM: passwordF, condition }).then((result) => {
-    //     res.send({ status: "ok", token: token, result })
-    // }).catch((err)=>{
-    //     res.send({ status: "refuse" })
-    // })
-   
- 
-    // user.find({ userName }, { UID: 1 }).then((result) => {
-    //     login.create({ "UID": result[0].UID, "logintime": addlikeTime }, (err, result) => {
-    //         if (err) {
-    //             console.log(err)
-    //         } else {
-    //         }
-    //     })
-    // })
+const login = require("../../tools/data/models/login");
+const { genrateToken } = require("../../utils/jwtCheck");
+const { argon, verify } = require("../../utils/argon");
 
-    // return;
+export function login(req, res) {
+  let { userName, password } = req.body;
+  let passwordF = argon(password);
+  let condition = 1;
+  // user.find({ userName, passWordM: passwordF, condition }).then((result) => {
+  //     res.send({ status: "ok", token: token, result })
+  // }).catch((err)=>{
+  //     res.send({ status: "refuse" })
+  // })
+  const userRepository = connectDatabase().getRepository(User);
+  const user = userRepository;
 
-
+  let result = user.find(userName, passwordF).then((res) => {
+    if (res != null) {
+      user.update(userName, { loginTime: now() });
+      return res;
+    }
+  });
+  user.close();
+  res.setHeader({
+    token: genrateToken(result),
+  });
+  res.send({
+    msg: null,
+    data: null,
+    status: "ok",
+  });
+  return;
 }
-
+export function signin(req, res) {
+  let { userName, password } = req.body;
+  let passwordF = argon(password);
+  const userRepository = connection.getRepository(User);
+  const user = new User();
+  let userId = ulid();
+  user = { userName, passwordF, userId, permissions: 0, loginTime: now() };
+  userRepository
+    .save(User)
+    .then((res) => {
+      res.setHeader;
+      res.send({ status: "ok", msg: null, data: null });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
