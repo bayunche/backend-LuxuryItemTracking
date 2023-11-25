507d38f2278b108f0ffcd7dfe5e6f5d71d91a1d2
bayunche <1035010167@qq.com>
2023-11-25 18:26:26 +0800

    feat    interactWithcontract complete

--- contracts/LuxuryItemTracking.sol
+++ contracts/LuxuryItemTracking.sol
@@ -31,11 +31,17 @@ contract LuxuryItemTracking is ERC721Enumerable, Ownable {
 
     string public baseTokenURI;
 
-    constructor(string memory baseURI, string memory name, string memory symbol) ERC721(name, symbol) {
+    constructor(
+        string memory baseURI,
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) {
         setBaseURI(baseURI);
         emit DebugMessage("Contract deployed successfully!");
     }
-event DebugMessage(string message);
+
+    event DebugMessage(string message);
+
     function _baseURI() internal view virtual override returns (string memory) {
         return baseTokenURI;
     }
@@ -44,68 +50,139 @@ event DebugMessage(string message);
         baseTokenURI = _baseTokenURI;
     }
 
-    function mintNFTs(string memory _name, uint256 _serialNumber, uint256 _productionDate) public payable {
+    function mintNFTs(
+        string memory _name,
+        uint256 _serialNumber,
+        uint256 _productionDate
+    ) public payable {
         uint totalMinted = _tokenIds.current();
 
-        require(totalMinted.add(1) <= MAX_SUPPLY, "This collection is sold out!");
-        require(_tokenIds.current() < MAX_SUPPLY, "This collection is sold out!");
-        require(_tokenIds.current() < MAX_SUPPLY, "This collection is sold out!");
-        require(_tokenIds.current() < MAX_SUPPLY, "This collection is sold out!");
-        require(_tokenIds.current() < MAX_SUPPLY, "This collection is sold out!");
-        require(_tokenIds.current() < MAX_SUPPLY, "This collection is sold out!");
-        require(!_exists(_serialNumber), "Item with this serial number already exists");
-        require(_tokenIds.current() < MAX_SUPPLY, "This collection is sold out!");
-        require(_tokenIds.current() < MAX_SUPPLY, "This collection is sold out!");
-        require(_tokenIds.current() < MAX_SUPPLY, "This collection is sold out!");
-        require(_tokenIds.current() < MAX_SUPPLY, "This collection is sold out!");
+        require(
+            totalMinted.add(1) <= MAX_SUPPLY,
+            "This collection is sold out!"
+        );
+        require(
+            _tokenIds.current() < MAX_SUPPLY,
+            "This collection is sold out!"
+        );
+        require(
+            _tokenIds.current() < MAX_SUPPLY,
+            "This collection is sold out!"
+        );
+        require(
+            _tokenIds.current() < MAX_SUPPLY,
+            "This collection is sold out!"
+        );
+        require(
+            _tokenIds.current() < MAX_SUPPLY,
+            "This collection is sold out!"
+        );
+        require(
+            _tokenIds.current() < MAX_SUPPLY,
+            "This collection is sold out!"
+        );
+        require(
+            !_exists(_serialNumber),
+            "Item with this serial number already exists"
+        );
+        require(
+            _tokenIds.current() < MAX_SUPPLY,
+            "This collection is sold out!"
+        );
+        require(
+            _tokenIds.current() < MAX_SUPPLY,
+            "This collection is sold out!"
+        );
+        require(
+            _tokenIds.current() < MAX_SUPPLY,
+            "This collection is sold out!"
+        );
+        require(
+            _tokenIds.current() < MAX_SUPPLY,
+            "This collection is sold out!"
+        );
+        require(
+            _tokenIds.current() < MAX_SUPPLY,
+            "This collection is sold out!"
+        );
+        require(
+            _tokenIds.current() < MAX_SUPPLY,
+            "This collection is sold out!"
+        );
         require(msg.value >= PRICE, "Not enough ether to purchase NFT.");
 
         _mintSingleNFT(_name, _serialNumber, _productionDate);
     }
 
-    function _mintSingleNFT(string memory _name, uint256 _serialNumber, uint256 _productionDate) private {
-      _tokenIds.increment();
-    LuxuryItem storage newItem = luxuryItems[_tokenIds.current()];
-    newItem.name = _name;
-    newItem.serialNumber = _serialNumber;
-    newItem.manufacturer = msg.sender;
-    newItem.productionDate = _productionDate;
+    function _mintSingleNFT(
+        string memory _name,
+        uint256 _serialNumber,
+        uint256 _productionDate
+    ) private {
+        _tokenIds.increment();
+        LuxuryItem storage newItem = luxuryItems[_tokenIds.current()];
+        newItem.name = _name;
+        newItem.serialNumber = _serialNumber;
+        newItem.manufacturer = msg.sender;
+        newItem.productionDate = _productionDate;
 
-    existingSerialNumbers[StringUtils.uintToString(_serialNumber)] = true;
+        existingSerialNumbers[StringUtils.uintToString(_serialNumber)] = true;
 
-    _safeMint(msg.sender, _tokenIds.current());
+        _safeMint(msg.sender, _tokenIds.current());
     }
 
-    function updateLogisticsInfo(uint256 _serialNumber, string memory _logisticsInfo) public onlyOwner {
+    function updateLogisticsInfo(
+        uint256 _serialNumber,
+        string memory _logisticsInfo
+    ) public onlyOwner {
         require(_exists(_serialNumber), "Item does not exist");
         luxuryItems[_serialNumber].logisticsInfo = _logisticsInfo;
     }
 
-    function updateSalesRecord(uint256 _serialNumber, string memory _salesRecord) public onlyOwner {
+    function updateSalesRecord(
+        uint256 _serialNumber,
+        string memory _salesRecord
+    ) public onlyOwner {
         require(_exists(_serialNumber), "Item does not exist");
         luxuryItems[_serialNumber].salesRecord = _salesRecord;
     }
 
     function certifyUser(uint256 _serialNumber) public {
         require(_exists(_serialNumber), "Item does not exist");
-        require(luxuryItems[_serialNumber].manufacturer == msg.sender, "Only manufacturer can certify users");
+        require(
+            luxuryItems[_serialNumber].manufacturer == msg.sender,
+            "Only manufacturer can certify users"
+        );
         luxuryItems[_serialNumber].certifiedUsers[msg.sender] = true;
     }
 
- function getItemDetails(uint256 _serialNumber) public view returns (string memory, address, uint256, string memory, string memory) {
-    require(_exists(_serialNumber), "Item does not exist");
-    
-    LuxuryItem storage item = luxuryItems[_serialNumber];
-    return (item.name, item.manufacturer, item.productionDate, item.logisticsInfo, item.salesRecord);
-}
+    function getItemDetails(
+        uint256 _serialNumber
+    )
+        public
+        view
+        returns (string memory, address, uint256, string memory, string memory)
+    {
+        require(_exists(_serialNumber), "Item does not exist");
+
+        LuxuryItem storage item = luxuryItems[_serialNumber];
+        return (
+            item.name,
+            item.manufacturer,
+            item.productionDate,
+            item.logisticsInfo,
+            item.salesRecord
+        );
+    }
 
     function isCertifiedUser(uint256 _serialNumber) public view returns (bool) {
         require(_exists(_serialNumber), "Item does not exist");
         return luxuryItems[_serialNumber].certifiedUsers[msg.sender];
     }
 
-  function _exists(uint256 _serialNumber) internal view override returns (bool)  {
-       return existingSerialNumbers[StringUtils.uintToString(_serialNumber)];
+    function _exists(
+        uint256 _serialNumber
+    ) internal view override returns (bool) {
+        return existingSerialNumbers[StringUtils.uintToString(_serialNumber)];
     }
-    
 }
