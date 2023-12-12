31384226f666dc262a1f65b5f45213b327281b45
bayunche <1035010167@qq.com>
2023-12-13 00:48:57 +0800

    async

--- migrations/1_LuxuryItemTracking_migration.js
+++ migrations/1_LuxuryItemTracking_migration.js
@@ -1,14 +1,10 @@
 const LuxuryItemTracking = artifacts.require("LuxuryItemTracking");
 const initialMirgation = artifacts.require("Migrations");
-const {Web3} = require('web3');
-const web3 = new Web3("http://127.0.0.1:8545"); // 替换为你的以太坊节点地址
-
 module.exports =async function (deployer, account) {
   deployer.deploy(initialMirgation);
   const baseURI = "127.0.0.1";
   const name = "LuxuryItems";
   const symbol = "LUX";
-  console.log("Deployer Account Balance:", web3.utils.fromWei(await web3.eth.getBalance(account[0]), "ether"), "ETH");
   deployer.deploy(LuxuryItemTracking, baseURI, name, symbol, {
     from: account[0],
   });
