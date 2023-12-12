c1c076c623aa53afbb4ed2bf34f47c448b373621
bayunche <1035010167@qq.com>
2023-12-12 23:25:27 +0800

    edit

--- migrations/1_LuxuryItemTracking_migration.js
+++ migrations/1_LuxuryItemTracking_migration.js
@@ -1,11 +1,11 @@
 const LuxuryItemTracking = artifacts.require("LuxuryItemTracking");
 const initialMirgation = artifacts.require("Migrations");
-module.exports = function (deployer) {
-
-
+module.exports = function (deployer, account) {
   deployer.deploy(initialMirgation);
   const baseURI = "127.0.0.1";
   const name = "LuxuryItems";
   const symbol = "LUX";
-  deployer.deploy(LuxuryItemTracking, baseURI, name, symbol);
-}
+  deployer.deploy(LuxuryItemTracking, baseURI, name, symbol, {
+    from: account[0],
+  });
+};
