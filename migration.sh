c248feded1fcaf0726a01e0f7cb23e9a61da39f1
bayunche <1035010167@qq.com>
2023-12-12 23:02:02 +0800

    migration

--- migration.sh
+++ migration.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
 
- service mysql start 
+service mysql start 
 
-ganache-cli -e 1200 --db /root/ddnwork/ganacheDB
+# 设置额外的参数
+extraArgs="-e 1200 --db /root/ddnwork/ganacheDB"
+
+# 启动 ganache-cli
+nohup ganache-cli $extraArgs > ganache-log.txt &
+
+truffle compile
+
+truffle migrate --network advanced
 
 pm2 start /root/ddnwork/client/server.js --name "ddn-client"
 
