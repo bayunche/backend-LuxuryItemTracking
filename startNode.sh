8de756e0f4976ab65a3c8a90841c1c037720fd91
jianqing.wang@ubtrobot.com <jianqing.wang@ubtrobot.com>
2023-12-18 09:29:18 +0800

    rewrite startNode.sh

--- startNode.sh
+++ startNode.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-nohup geth --datadir ./node1/data --port 2001  --http --http.port 8548 --allow-insecure-unlock    --rpc.enabledeprecatedpersonal  > node1output.log 2>&1 &
+nohup geth --datadir ./node1/data --port 2001   --http --http.port 8548 --allow-insecure-unlock    --rpc.enabledeprecatedpersonal  > node1output.log 2>&1 &
 
-nohup geth --datadir ./node2/data --port 2002  --http --http.port 8549 --allow-insecure-unlock    --rpc.enabledeprecatedpersonal  > node2output.log 2>&1 &
+nohup geth --datadir ./node2/data --port 2002  --authrpc.port 8560 --http --http.port 8549 --allow-insecure-unlock    --rpc.enabledeprecatedpersonal  > node2output.log 2>&1 &
 
-nohup geth --datadir ./node3/data --port 2003  --http --http.port 8550 --allow-insecure-unlock    --rpc.enabledeprecatedpersonal   > node3output.log 2>&1 &
+nohup geth --datadir ./node3/data --port 2003 --authrpc.port 8559  --http --http.port 8550 --allow-insecure-unlock    --rpc.enabledeprecatedpersonal   > node3output.log 2>&1 &
