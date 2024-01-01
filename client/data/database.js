0a86316128fd465bd16def4f6f3efe213ba54c5e
bayunche <1035010167@qq.com>
2024-01-01 22:23:38 +0800

    change sql password

--- client/data/database.js
+++ client/data/database.js
@@ -3,8 +3,8 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
     dialect: "mysql",
     host: "localhost",
-    username: "root",
-    password: "123456",
+    username: "testuser",
+    password: "12345678",
     database: "ddn",
   });
 
