4971326490239e48457f7bd75bbe65862bdb818e
bayunche <1035010167@qq.com>
2024-02-16 00:17:58 +0800

    feat bug fix

--- client/router/user/router.js
+++ client/router/user/router.js
@@ -4,7 +4,7 @@ const user = require("./user");
 
 router.get("/signupUser", user.createUserPrivateKey);
 
-router.post("/getUserInfo", user.getUserInfo);
+router.get("/getUserInfo", user.getUserInfo);
 
 
 module.exports = router;
