278d33427f9ddb9165dc7293f8c35227a8e41073
bayunche <1035010167@qq.com>
2024-02-16 12:28:11 +0800

    feat 优化jwt

--- client/middleWare/jwtCheck.js
+++ client/middleWare/jwtCheck.js
@@ -1,15 +1,24 @@
 const { verifyToken } = require("../utils/jwtCheck");
-exports.jwtcheck =async (req, res, next) => {
-  const token = req.headers.authorization;
-  if (token == null) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-  let jwtToken = token.replace("Bearer ", "");
-  let decodedToken =await verifyToken(jwtToken);
-  if (decodedToken == null) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  } else {
+// const { verifyToken } = require('./your-token-module');
+
+exports.jwtcheck = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const jwtToken = token.replace("Bearer ", "");
+    const decodedToken = await verifyToken(jwtToken);
+
+    if (!decodedToken) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
     req.userId = decodedToken.userId;
     next();
+  } catch (error) {
+    console.error("Error in jwtcheck middleware:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
