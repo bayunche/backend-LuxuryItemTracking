6823f40c69f5face0bf951e6e7ad956850f0b447
jianqing.wang@ubtrobot.com <jianqing.wang@ubtrobot.com>
2024-03-05 19:12:36 +0800

    test

--- client/router/login/login.js
+++ client/router/login/login.js
@@ -4,8 +4,7 @@ const { ulid } = require("ulid");
 const User = require("../../data/user");
 const sequelize = require("../../data/database");
 const { generateToken } = require("../../utils/jwtCheck");
-const { argon,verifyArgon } = require("../../utils/argon");
-
+const { argon, verifyArgon } = require("../../utils/argon");
 
 exports.login = async (req, res) => {
   try {
@@ -14,35 +13,35 @@ exports.login = async (req, res) => {
     const user = await User.findOne({ where: { userName } });
 
     if (!user) {
-      return res.send({ status: "refuse" });
+      res.send({ status: "refuse", msg: "用户名或密码错误" });
+      return;
     }
-
-    const isPasswordValid = await verifyArgon(user.passwordF, hashedPassword);
-
+    const isPasswordValid = await verifyArgon(user.passwordF, password);
     if (!isPasswordValid) {
-      return res.send({ status: "refuse" });
+      res.send({ status: "refuse", msg: "用户名或密码错误" });
+      return;
     }
-
     const token = generateToken(user.userId);
 
     res.setHeader("Authorization", `Bearer ${token}`);
     res.send({ status: "ok", msg: null, data: null });
   } catch (error) {
     console.error("Error in login:", error);
-    res.status(500).send({ status: "error", msg: "Internal Server Error", data: null });
+    res
+      .status(500)
+      .send({ status: "error", msg: "Internal Server Error", data: null });
   }
 };
 
 exports.signup = async (req, res) => {
   try {
     if (Object.keys(req.body).length === 0) {
-      return res.send({ err: "empty", data: null });
+      return res.send({ status: "refuse",msg:"参数错误", data: null });
     }
-
     const { userName, password } = req.body;
     const hashedPassword = await argon(password);
     const userId = ulid();
-
+    console.log(hashedPassword)
     const user = await User.create({
       userName,
       passwordF: hashedPassword,
@@ -55,6 +54,8 @@ exports.signup = async (req, res) => {
     res.send({ status: "ok", msg: null, data: null });
   } catch (error) {
     console.error("Error in signup:", error);
-    res.status(500).send({ status: "error", msg: "Internal Server Error", data: null });
+    res
+      .status(500)
+      .send({ status: "error", msg: "Internal Server Error", data: null });
   }
 };
