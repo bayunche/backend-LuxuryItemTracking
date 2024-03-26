c9e7142ab4b3e4853fd5442dd3636f6d82962f9b
jianqing.wang@ubtrobot.com <jianqing.wang@ubtrobot.com>
2024-03-26 10:29:13 +0800

    debug

--- client/utils/randomInt.js
+++ client/utils/randomInt.js
@@ -5,9 +5,6 @@ function generateSecureRandomNumber() {
   const timestamp = BigInt(Date.now());
   const randomPart = BigInt(Math.floor(Math.random() * 10000)); // 生成一个较小的随机数
   return (timestamp << 16n) + randomPart; // 将时间戳和随机数结合
-
- 
 }
 
-
-module.exports= generateSecureRandomNumber;
+module.exports = generateSecureRandomNumber;
