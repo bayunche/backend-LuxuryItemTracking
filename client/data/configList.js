96e3174ee113fe01dfe046ad5a4a59f446c3e11e
bayunche <1035010167@qq.com>
2024-04-08 08:33:43 +0800

    debug

--- client/data/configList.js
+++ client/data/configList.js
@@ -1,6 +1,5 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("./database.js");
-
 const configList = sequelize.define("configList", {
   id: {
     type: DataTypes.INTEGER,
