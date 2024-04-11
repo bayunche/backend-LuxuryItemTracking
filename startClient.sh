93d09b205016d0861f8ae0fc659975a2f88ca6fa
bayunche <1035010167@qq.com>
2024-04-11 21:24:14 +0800

    debug

--- startClient.sh
+++ startClient.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-pm2 start /root/ddnwork/client/server.js --name "ddn-client" --output /root/ddnwork/client/logs/out.log --error /root/ddnwork/client/logs/err.log --time
+pm2 start ./client/server.js --name "ddn-client" --output ./client/logs/out.log --error ./client/logs/err.log --time
 
 pm2 list 
