08e0ab653b23d32d02245ab0bedf0f9a624d555b
bayunche <1035010167@qq.com>
2024-04-13 23:44:26 +0800

    debug

--- client/router/user/router.js
+++ client/router/user/router.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const user = require("./user");
 
-router.get("/signupUser", user.createUserPrivateKey);
-router.post("/certifiedUser", user.certifiedUser);
+router.post("/signupUser", user.createUserPrivateKey);
+router.get("/certifiedUser", user.certifiedUser);
 router.get("/getUserInfo", user.getUserInfo);
 router.post('/editPassword',user.editUserPassword)
 router.post('/editUserInfo',user.editUserInfo)
