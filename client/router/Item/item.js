cef12b1e92ab113a35bb678586ebde829f2ab8b9
bayunche <1035010167@qq.com>
2024-04-15 19:20:23 +0800

    debug

--- client/router/Item/item.js
+++ client/router/Item/item.js
@@ -183,6 +183,7 @@ exports.getSalesList = async (req, res) => {
 
 exports.deleteItem = async (req, res) => {
   let { itemId } = req.params;
+  console.log(itemId)
   if (itemId == null) {
     return res.send({
       status: "refuse",
